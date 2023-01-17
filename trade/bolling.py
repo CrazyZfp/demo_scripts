fa90cdbae0687bf7796e2cbaf7acb487aaf6d8e1
zfp <zhengfeipeng@mail.i-i.ai>
2023-01-17 14:09:01 +0800

    update bolling.py

--- trade/bolling.py
+++ trade/bolling.py
@@ -1,31 +1,62 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-def boll_bands(data, ndays,k):
-    ma = pd.Series(np.round(data['Close'].rolling(ndays).mean(), 2), name=f'MA{ndays}')  # 计算nday均线
+
+def boll_bands(df, n, k):
+    ma = pd.Series(np.round(df['Close'].rolling(n).mean(), 2), name=f'MA{n}')
+    df = df.join(ma)
+
     # pandas.std() 默认是除以n-1 的，即是无偏的，如果想和numpy.std() 一样有偏，需要加上参数ddof=0
     # 此处添加ddof的原因是wind和yahoo的计算均采用的有偏值进行的计算
-    std = pd.Series(np.round(data['Close'].rolling(ndays).std(ddof=0), 2))  # 计算nday标准差，有偏
-    b1 = ma + (k * std)  # 此处的2就是Standard Deviations
-    B1 = pd.Series(b1, name='up')
-    data = data.join(ma)  # 上边不写name 这里报错
-    data = data.join(B1)
+    std = pd.Series(np.round(df['Close'].rolling(n).std(ddof=0), 2),
+                    name=f'std{n}')
+    df = df.join(std)
 
-    b2 = ma - (k * std)
-    B2 = pd.Series(b2, name='low')
-    data = data.join(B2)
+    b_up = pd.Series(ma + (k * std), name='up')
+    df = df.join(b_up)
 
-    data['percent']= abs((data['Close'] - ma)/std)
+    b_low = pd.Series(ma - (k * std), name='low')
+    df = df.join(b_low)
 
-    return data
+    return df
+
+
+def draw_figure(df, n):
+    ax = df.plot(y=['low', 'up', 'Close', f'MA{n}'], x_compat=True)
+
+    ax2 = ax.twinx()
+    ax2.bar(x=df.index, height=df.percent, color=(0.5, 0.5, 0.5, 0.2))
+
+    plt.show()
+
+
+def df_init():
+    df = pd.Series([100, 45, 67, 8, 99, 99, 76, 32, 45, 66], name='Close')
+    df = pd.DataFrame(df)  # 得到的数据中index直接就是Date
+
+    date = pd.date_range(start='2020-09-09', periods=10, name='date')
+    df['date'] = date
+    df.set_index('date', inplace=True)
+    return df
+
+
+def bb_percent_calculate(bb, n):
+    bb['percent'] = abs((bb['Close'] - bb[f'MA{n}']) / bb[f'std{n}'])
+    return bb
+
+
+def main():
+    n = 5
+    k = 2
+
+    df = df_init()
+    df = boll_bands(df, n, k)
+    df = bb_percent_calculate(df, n)
+    print(df.loc[:, ['low', 'up', 'percent', 'Close', f'MA{n}']])
+
+    draw_figure(df, n)
 
 
 if __name__ == '__main__':
-	# pandas调用yahoo财经的股票数据，此处以纳斯达克指数为例计算
-    data = pd.Series([100,45,67,8,99,99,76,32,45,66], name='Close')
-    data = pd.DataFrame(data)  # 得到的数据中index直接就是Date
-    n = 5
-    k= 2
-
-    bb = boll_bands(data, n,k)
-    print(bb.loc[:, ['low', 'up', 'percent', 'Close', f'MA{n}']])
+    main()
